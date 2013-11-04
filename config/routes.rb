41c4803ef3b23c8a500d2807bcdf8a8ec8c31637
Steve Herrod <herrod@gmail.com>
2013-11-03 16:20:04 -0800

    about page and basic css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Drinkwater::Application.routes.draw do
   root "pages#home"
+
+  get "about" => "pages#about"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
